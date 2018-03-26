c0badd9a7170162cd368c321ed2d39daad95a72e
j_j_c@hotmail.com <36175703+joshzcold@users.noreply.github.com>
2018-03-26 10:40:02 -0600

    Made the main activity into menu with a button that calls the five second game activity. Now we can add more game modes.

--- app/src/main/java/com/jcold/speedytapper/MainActivity.kt
+++ app/src/main/java/com/jcold/speedytapper/MainActivity.kt
@@ -1,92 +1,28 @@
 package com.jcold.speedytapper
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.os.CountDownTimer
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var counter = 0
-    var blankString = ""
-    var startTimer: CountDownTimer? = null
-    var stopTimer: CountDownTimer? = null
-    var remaining_time: TextView? = null
-    var countdown_text: TextView? = null
-    var timerIsStarted = false
-    var highscoreText: TextView? = null
-    var highScore = 0
-    var currentscoreText: TextView? = null
-
+    var menuFiveSecondButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bindViews()
-        gameFiveSeconds()
+        menuFiveSecondButton?.setOnClickListener { fiveSecondGame() }
+    }
+
+    private fun fiveSecondGame() {
+        val intent = Intent(this, FiveSecondGameActivity::class.java)
+        startActivity(intent)
     }
 
     private fun bindViews() {
-        remaining_time = findViewById(R.id.countdown_timer)
-        countdown_text = findViewById(R.id.countdown_timer)
-        highscoreText = findViewById(R.id.high_score)
-        currentscoreText = findViewById(R.id.current_score)
-
-
+        menuFiveSecondButton = findViewById(R.id.menu_five_seconds)
     }
 
-    private fun highScoreSetter(){
-        if (highScore < counter){
-           highScore = counter
-                   highscoreText?.setText("High Score: "+blankString+counter)
-        }
-    }
-
-    private fun currentScoreSetter(){
-        currentscoreText?.setText("Score: "+counter)
-    }
-
-    private fun gameTimerStart(){
-        startTimer = object : CountDownTimer(5000, 1) {
-            override fun onTick(millisUntilFinished: Long) {
-                timerIsStarted = true
-                var seconds = (millisUntilFinished / 1000) %60
-                var milli = (millisUntilFinished % 1000)
-                countdown_text?.setText("Seconds: "+ String.format("%d.%02d", seconds, milli))
-            }
-
-            override fun onFinish() {
-                timerIsStarted = false
-                countdown_text?.setText("Seconds: 5")
-                highScoreSetter()
-                currentScoreSetter()
-                counter =0
-                number_increasing.setText(blankString+counter)
-            }
-        }.start()
-    }
-
-    private fun gameFiveSeconds() {
-
-        button_reset_timer.setOnClickListener{
-
-            highScoreSetter()
-            currentScoreSetter()
-            counter =0
-            number_increasing.setText(blankString+counter)
-            startTimer?.cancel()
-            countdown_text?.setText("Seconds: 5")
-            timerIsStarted = false
-        }
-
-        myButton.setOnClickListener{
-            if (!timerIsStarted){
-                gameTimerStart()
-            }
-            counter += 1
-            number_increasing.setText(blankString+counter)
-        }
-    }
 }
